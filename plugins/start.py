64679204d96e5699b09ae9ff7c70d27394ae5b88
xrnvx <74348714+xrnvx@users.noreply.github.com>
2025-07-08 19:33:58 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -158,7 +158,7 @@ async def start_command(client: Client, message: Message):
     else:
         reply_markup = InlineKeyboardMarkup(
             [
-                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/Nova_Flix/50")],
+                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/willslv")],
 
     [
                     InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data = "about"),

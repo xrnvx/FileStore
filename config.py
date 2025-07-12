9286d82d099b0e3f804cbf9757b913ff4d0d6a9e
xrnvx <74348714+xrnvx@users.noreply.github.com>
2025-07-12 18:54:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -36,8 +36,8 @@ FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/CodeflixSupport")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
-START_PIC = os.environ.get("START_PIC", "")
-FORCE_PIC = os.environ.get("FORCE_PIC", "")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/4e0de2960a9e416ebd0d4-fd2e74f268f4fdee3d.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://graph.org/file/4e0de2960a9e416ebd0d4-fd2e74f268f4fdee3d.jpg")
 #--------------------------------------------
 
 #--------------------------------------------

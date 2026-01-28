f419a910bd02b075e2badcf2fc6438e6ca20fbc9
xrnvx <74348714+xrnvx@users.noreply.github.com>
2026-01-28 12:45:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
 PORT = '5010'
-OWNER_ID = 6497757690
+OWNER_ID = 7859564193
 
 MSG_EFFECT = 5046509860389126442
 
